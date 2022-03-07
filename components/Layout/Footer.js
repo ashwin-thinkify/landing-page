065d56930d4cc416d9f2d2ebfbd3ad311deb0fa0
ashwin-thinkify <ak@thinkify.io>
2022-03-07 11:31:51 +0530

    Added the footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -8,19 +8,19 @@ const Footer = () => {
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <LogoVPN className="w-auto bg-indigo-700 mb-6" />
+          {/* <LogoVPN className="w-auto bg-indigo-700 mb-6" /> */}
           <p className="mb-4">
             <strong className="font-medium">Thinkify.io</strong> Provide Technology Enabled Solution Delivery.
           </p>
           <div className="flex w-full mt-2 mb-8 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Facebook className="h-6 w-6" />
+              {/* <Facebook className="h-6 w-6" /> */}
             </div>
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Twitter className="h-6 w-6" />
+              {/* <Twitter className="h-6 w-6" /> */}
             </div>
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Instagram className="h-6 w-6" />
+              {/* <Instagram className="h-6 w-6" /> */}
             </div>
           </div>
           <p className="text-gray-400">©2021 Thinkify.io</p>
