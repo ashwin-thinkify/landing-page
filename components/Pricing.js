609d89209883eed36e44f7ac2d692aed7ceaba39
ashwin-thinkify <ak@thinkify.io>
2022-03-07 12:18:23 +0530

    Updated the UI

--- components/Pricing.js
+++ components/Pricing.js
@@ -13,16 +13,6 @@ const Pricing = () => {
     >
       <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
         <div className="flex flex-col w-full my-16">
-          <h3 className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed w-9/12 sm:w-6/12 lg:w-4/12 mx-auto">
-            Huge Global Network of Fast VPN{" "}
-          </h3>
-          <p className="leading-normal  mx-auto my-2 w-10/12 sm:w-7/12 lg:w-6/12">
-            See LaslesVPN everywhere to make it easier for you when you move
-            locations.
-          </p>
-          <div className="py-12 w-full px-8 mt-16">
-            {/* <img src={Maps} className="w-full h-auto" /> */}
-          </div>
           <div className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap">
             {/* <Netflix className="h-18 w-auto" /> */}
             <img
@@ -54,11 +44,11 @@ const Pricing = () => {
         </div>
         <div className="flex flex-col w-full my-16" id="testimoni">
           <h3 className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
-            Trusted by Thousands of Happy Customer{" "}
+            Trusted by Thousands of Employees{" "}
           </h3>
           <p className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12">
-            These are the stories of our customers who have joined us with great
-            pleasure when using this crazy feature.
+            These are the stories of our employee who have joined us with great
+            pleasure when we put it our in the world.
           </p>
           <div className="w-full flex flex-col py-12">
             <Testimoni />
