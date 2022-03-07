acc9cd1553d9f7581fd1417849ea6aa3ab0a31c7
ashwin-thinkify <ak@thinkify.io>
2022-03-07 20:05:11 +0530

    Added the links

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -50,6 +50,24 @@ const Header = () => {
             </LinkScroll>
             <LinkScroll
               activeClass="active"
+              to="clients"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("clients");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "clients"
+                  ? " text-indigo-700 animation-active "
+                  : " text-black-500 hover:text-indigo-700 ")
+              }
+            >
+              Clients
+            </LinkScroll>
+            <LinkScroll
+              activeClass="active"
               to="feature"
               spy={true}
               smooth={true}
@@ -66,25 +84,7 @@ const Header = () => {
             >
               Feature
             </LinkScroll>
-            <LinkScroll
-              activeClass="active"
-              to="pricing"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              onSetActive={() => {
-                setActiveLink("pricing");
-              }}
-              className={
-                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
-                (activeLink === "pricing"
-                  ? " text-indigo-700 animation-active "
-                  : " text-black-500 hover:text-indigo-700 ")
-              }
-            >
-              Pricing
-            </LinkScroll>
-            <LinkScroll
+            {/* <LinkScroll
               activeClass="active"
               to="testimoni"
               spy={true}
@@ -101,7 +101,7 @@ const Header = () => {
               }
             >
               Testimonial
-            </LinkScroll>
+            </LinkScroll> */}
           </ul>
           {/* <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
             <Link href="/">
@@ -152,6 +152,38 @@ const Header = () => {
             </LinkScroll>
             <LinkScroll
               activeClass="active"
+              to="clients"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("clients");
+              }}
+              className={
+                "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
+                (activeLink === "clients"
+                  ? "  border-indigo-700 text-indigo-700"
+                  : " border-transparent ")
+              }
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Clients
+            </LinkScroll>
+            <LinkScroll
+              activeClass="active"
               to="feature"
               spy={true}
               smooth={true}
@@ -182,39 +214,8 @@ const Header = () => {
               </svg>
               Feature
             </LinkScroll>
-            <LinkScroll
-              activeClass="active"
-              to="pricing"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              onSetActive={() => {
-                setActiveLink("pricing");
-              }}
-              className={
-                "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
-                (activeLink === "pricing"
-                  ? "  border-indigo-700 text-indigo-700"
-                  : " border-transparent ")
-              }
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Pricing
-            </LinkScroll>
-            <LinkScroll
+            
+            {/* <LinkScroll
               activeClass="active"
               to="testimoni"
               spy={true}
@@ -245,7 +246,7 @@ const Header = () => {
                 />
               </svg>
               Testimonial
-            </LinkScroll>
+            </LinkScroll> */}
           </ul>
         </div>
       </nav>
--- components/Pricing.js
+++ components/Pricing.js
@@ -9,7 +9,7 @@ const Pricing = () => {
   return (
     <div
       className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
-      id="pricing"
+      id="clients"
     >
       <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
         <div className="flex flex-col w-full my-16">
